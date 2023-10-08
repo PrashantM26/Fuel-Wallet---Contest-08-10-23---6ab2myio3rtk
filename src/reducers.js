402c304465dbecca9d9a703ca077dea8a77ecd70
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-08 10:36:43 +0530

    Update reducers.js

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,29 @@
 import { v4 as uuidv4 } from "uuid";
 
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  switch (action.type) {
+      
+    case 'ADD_USER' :
+      const newUser = {
+        id: uuidv4(),
+        name: action.name,
+      }
+      return {
+        ...state,
+        walletBalance: state.walletBallance + 10,
+        users: [...state.users, newUser],
+      };
+
+    case 'HANDLE_FUEL' :
+      if(state.walletBalance >= 50) {
+        return {
+          ...state,
+          walletBalance: state.walletBalance - 50,
+        };
+      }
+
+      deafult : 
+        return state;
+};
 
 export default reducer;
